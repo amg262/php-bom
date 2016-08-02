2e9b9a25967b7c790c7939d50bd3d6f326481046
Andrew Gunn <andrewmgunn26@gmail.com>
2016-08-02 06:13:22 -0500

    ggg

--- classes/Part.php
+++ classes/Part.php
@@ -5,35 +5,14 @@
  * Date: 8/2/16
  * Time: 1:34 AM
  */
-include('iPart.php');
 
 
-class Part implements iPart
+class Part
 {
-    private $part_no;
+    private $id, $part_no, $name, $desc, $cost;
+    private $attr = array();
+    private Vendor $vendor;
 
-    /**
-     * Part constructor.
-     */
-    public function __construct()
-    {
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPartNo()
-    {
-        return $this->part_no;
-    }
-
-    /**
-     * @param mixed $part_no
-     */
-    public function setPartNo($part_no)
-    {
-        $this->part_no = $part_no;
-    }
 
 
 
